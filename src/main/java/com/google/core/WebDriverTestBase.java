cffa921b92c777c94abd9c5a3f271079751bdcb9
ZIPMERIEN <viktori.iaropovetska@gmail.com>
2018-02-14 19:10:13 +0200

    Another try

--- src/main/java/com/google/core/WebDriverTestBase.java
+++ src/main/java/com/google/core/WebDriverTestBase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverTestBase {
